30ae8730fd23107498ec31114fa6c719eb492f45
Malik <malikabdulaziz51@gmail.com>
2019-11-21 14:41:37 +0700

    get all

--- models/unit.php
+++ models/unit.php
@@ -9,6 +9,47 @@ class Unit
         $this->db = $db;
     }
 
+    public function get($tablename)
+    {
+        $query = "SELECT
+           *
+          FROM
+             $tablename
+          ORDER BY
+            id ASC";
+
+        $result = $this->db->execute($query);
+
+        $num = $result->rowCount();
+
+        if ($num > 0) {
+
+            $data_arr = array();
+
+            while ($row = $result->fetchRow()) {
+                extract($row);
+
+                $data_item = array(
+                    'id' => $id,
+                    'organization_id' => $organization_id,
+                    'organization_name' => $organization_name,
+                    'organization_code' => $organization_code,
+                    'parent_id' => $parent_id,
+                    'name' => $name,
+                    'code' => $code,
+                );
+
+                array_push($data_arr, $data_item);
+                $msg = $data_arr;
+            }
+
+        } else {
+            $msg = 'Data Kosong';
+        }
+
+        return $msg;
+    }
+
     public function getByParent($tablename, $parent_id)
     {
         $query = "SELECT
