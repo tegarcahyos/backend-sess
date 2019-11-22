fbdddf6a6886927674d7d90eee37dfaf1b207956
Malik <malikabdulaziz51@gmail.com>
2019-11-22 15:03:38 +0700

    test config form

--- models/config_form.php
+++ models/config_form.php
@@ -72,6 +72,7 @@ class ConfigForm
         //
         $request = json_decode($data);
         // die(json_encode($request));
+        die(print_r($request));
         $form_config = $request['form_config'][0]->form;
         // $form_id = $request[0]->form_id;
         // $form_name = $request[0]->form_name;
