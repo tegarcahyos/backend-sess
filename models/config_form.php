5bf1c1b8c7f26bb3ccab4e68f35450730ee127f4
Malik <malikabdulaziz51@gmail.com>
2019-11-25 13:07:12 +0700

    get implode

--- models/config_form.php
+++ models/config_form.php
@@ -57,7 +57,7 @@ class ConfigForm
 
         $data_item = array(
             'id' => $id,
-            'form_config' => json_decode($form_config),
+            'data_cfg' => json_decode($form_config),
             // 'form_id' => $form_id,
             // 'form_name' => $form_name,
 
@@ -71,7 +71,6 @@ class ConfigForm
         // get data input from frontend
         $data = file_get_contents("php://input");
         //
-        // $request = json_decode($data);
 
         $data_form = $data;
 
