87afd81980b3fd09c33c1dab876b16b9158a463d
Malik <malikabdulaziz51@gmail.com>
2019-11-25 11:16:22 +0700

    get implode

--- models/config_form.php
+++ models/config_form.php
@@ -79,7 +79,7 @@ class ConfigForm
         // $query = "INSERT INTO $tablename (form_id, form_name, form_config)";
         // $query .= " VALUES ($form_id , '$form_name', '$form_config')";
         // $implode = implode(" ", $data);
-        die(json_encode($data));
+        die(json_encode($data) + "");
 
         $query = "INSERT INTO $tablename (form_config)";
         $query .= " VALUES ('[$form_config]'::jsonb)";
