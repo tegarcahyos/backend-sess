60258244b3f7e0a9562105a7d13a02e977642f51
Malik <malikabdulaziz51@gmail.com>
2019-11-25 14:00:48 +0700

    insert form

--- models/config_form.php
+++ models/config_form.php
@@ -95,7 +95,7 @@ class ConfigForm
         $form_submit_type = $request[0]->type;
         $form_name = $request[0]->name;
         $query = "INSERT INTO $tablename (form_submit_type, form_name)";
-        $query .= " VALUES ($form_submit_type , '$form_name')";
+        $query .= " VALUES ('$form_submit_type' , '$form_name')";
         // die($query);
         return $this->db->execute($query);
 
