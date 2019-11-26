5ff3d742224bded49ee5297840765ea9ed0d73fa
Malik <malikabdulaziz51@gmail.com>
2019-11-26 13:29:01 +0700

    api user_unit

--- models/user_unit.php
+++ models/user_unit.php
@@ -86,7 +86,7 @@ class UserUnit
              $tablename
               WHERE parent_id =  $parent_id ";
 
-        die($query);
+        // die($query);
         $result = $this->db->execute($query);
 
         $num = $result->rowCount();
