bc7ed5bb657bc19b0cf84757f06708484b9579ed
Malik <malikabdulaziz51@gmail.com>
2019-11-26 11:15:19 +0700

    api object all

--- models/db.php
+++ models/db.php
@@ -89,7 +89,7 @@ class DB
         }
 
         $query_real = str_replace("%20", " ", $query);
-        // die($query_real);
+        die($query_real);
         $result = $this->db->execute($query_real);
 
         $num = $result->rowCount();
