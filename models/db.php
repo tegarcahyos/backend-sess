9ca87630e1da906e32d7f9886de7b4984699e86d
Malik <malikabdulaziz51@gmail.com>
2019-11-26 11:07:37 +0700

    api object all

--- models/db.php
+++ models/db.php
@@ -26,7 +26,7 @@ class DB
 
                 $data_item = array(
                     'id' => $id,
-                    'values' => $values,
+                    'values' => json_decode($values),
                 );
 
                 array_push($data_arr, $data_item);
