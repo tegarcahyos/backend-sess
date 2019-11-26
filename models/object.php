3dec6096fd42a91fda0bd63bfc6f9ebbd23e79ec
Malik <malikabdulaziz51@gmail.com>
2019-11-26 15:56:51 +0700

    api object form

--- models/object.php
+++ models/object.php
@@ -129,9 +129,9 @@ class Objects
         extract($row);
         $data = json_encode($row);
         $name = $data->name;
+        die($name);
         $name = strtolower($name);
         $name = str_replace(" ", "_", $name);
-        die($name);
         $query = "ALTER TABLE data_$name RENAME TO deleted_$name";
         return $this->db->execute($query);
     }
