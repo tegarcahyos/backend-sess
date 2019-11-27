a7756db46954f1c7396fa88d0c28db1c173cb159
Malik <malikabdulaziz51@gmail.com>
2019-11-27 09:45:12 +0700

    api table

--- models/config_table.php
+++ models/config_table.php
@@ -53,7 +53,7 @@ class ConfigTable
 
     public function findById($id, $tablename)
     {
-        $query = 'SELECT * FROM ' . $tablename . ' WHERE id = ' . $id . "";
+        $query = "SELECT * FROM $tablename WHERE id = '$id'";
         $result = $this->db->execute($query);
         $row = $result->fetchRow();
         extract($row);
