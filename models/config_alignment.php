d92f7a5ce351f52c397770a248a27fb475147b89
Malik <malikabdulaziz51@gmail.com>
2019-11-28 16:55:31 +0700

    update api object

--- models/config_alignment.php
+++ models/config_alignment.php
@@ -92,7 +92,7 @@ class ConfigAlignment
         // die(json_decode($data));
         $name = $request[0]->name;
         $alignment = json_encode($request[0]->data);
-        $query = "UPDATE  $tablename SET name = '$name', alignment = '$alignment' WHERE id = $id";
+        $query = "UPDATE  $tablename SET name = '$name', alignment = '$alignment' WHERE id = '$id'";
         // die($query);
         return $this->db->execute($query);
     }
