8f2d6debdb546f5ccf76d7424c1a3c14152e19e6
Malik <malikabdulaziz51@gmail.com>
2019-12-05 23:38:01 +0700

    tes expire token

--- router.php
+++ router.php
@@ -132,8 +132,8 @@ class Router
     // REQUEST
     public function request()
     {
-        // $passed = $this->check_token();
-        // if($passed == true){
+        $passed = $this->check_token('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJUSEVfSVNTVUVSIiwiYXVkIjoiVEhFX0FVRElFTkNFIiwiaWF0IjoxNTc1NDQzMDkzLCJuYmYiOjE1NzU0NDMxMDMsImV4cCI6MTU3NTQ0MzE1MywiZGF0YSI6eyJuYW1lIjoiQXNkYXIiLCJ1c2VybmFtZSI6ImFzZGFyMTIzIn19.GFgkoZu-7X0fYiZePTzsYUoQQXF--xd28AuzltE_KpI');
+        if($passed == true){
         header("Access-Control-Allow-Origin: * ");
         $explodeUrl = explode('/', $this->url);
         $explodeUrl = array_slice($explodeUrl, 3);
@@ -409,6 +409,6 @@ class Router
             $this->msg(203, $th, "Terjadi Kesalahan");
         }
     }
-    // }
+    }
     
 }
