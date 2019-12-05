afddebee9bfefb1f96d4389cc7e52ad335f2c8d2
Malik <malikabdulaziz51@gmail.com>
2019-12-05 22:47:30 +0700

    tes expire token

--- login.php
+++ login.php
@@ -92,7 +92,7 @@ class Login
                     "message" => "Successful login.",
                     "name" => $name,
                     "username" => $username,
-                    "expireAt" => date($expire_claim),
+                    "expireAt" => date('Y-m-d', $expire_claim),
                     "role_id" => $role_id,
                     "role_name" => $role_name,
                     "unit_id" => $unit_id,
