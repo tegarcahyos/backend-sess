8c2e98409b4e52e65f8d4ca6157e983b2158b5c5
Malik <malikabdulaziz51@gmail.com>
2019-12-10 11:20:01 +0700

    page data tes

--- models/page_data.php
+++ models/page_data.php
@@ -31,7 +31,9 @@ class PageData
 
                 $data_item = array(
                     'id' => $id,
-                    'data' => json_decode($data),
+                    'page_id' => $page_id,
+                    'page_name' => $page_name,
+                    'data' => $data,
 
                 );
 
@@ -55,62 +57,23 @@ class PageData
 
         $data_item = array(
             'id' => $id,
-            'data' => json_decode($data),
+            'page_id' => $page_id,
+            'page_name' => $page_name,
+            'data' => $data,
         );
         return $data_item;
-    }
-
-    public function select_where_get($attr, $val, $tablename)
-    {
-
-        $query = "SELECT values FROM $tablename WHERE ";
-        $values = explode('AND', $val);
-        $attr = explode('AND', $attr);
-        for ($i = 0; $i < count($attr); $i++) {
-            if ($i == 0) {
-
-            } else {
-                $query .= " AND ";
-            }
-            $query .= "values @> '{\"" . $attr[$i] . "\": \"" . $values[$i] . "\"}'";
-
-        }
-
-        $query_real = str_replace("%20", " ", $query);
-        // die($query_real);
-        $result = $this->db->execute($query_real);
-
-        $num = $result->rowCount();
-
-        if ($num > 0) {
-            $arr = array();
-
-            while ($row = $result->fetchRow()) {
-                extract($row);
-
-                array_push($arr, json_decode($values));
-            }
-
-            $item = array(
-                'data' => $arr,
-            );
-
-            // echo json_encode($item);
-        } else {
-            echo json_encode(
-                array('message' => 'data tidak ditemukan')
-            );
-        }
-        return $item;
     }
 
     public function insert($tablename)
     {
         // get data input from frontend
         $data = file_get_contents("php://input");
-
-        $query = "INSERT INTO $tablename (data)";
-        $query .= " VALUES ('$data')";
+        $request = json_decode($data);
+        $page_id = $request[0]->page_id;
+        $page_name = $request[0]->page_name;
+        $data = $request[0]->data;
+        $query = "INSERT INTO $tablename (page_id, page_name, data)";
+        $query .= " VALUES ($page_id, '$page_name','$data')";
         // die($query);
         return $this->db->execute($query);
 
@@ -120,36 +83,14 @@ class PageData
     {
         // get data input from frontend
         $data = file_get_contents("php://input");
-        //
-        // die(json_encode($request));
-        $data = $request->data;
+        $request = json_decode($data);
+        $page_id = $request[0]->page_id;
+        $page_name = $request[0]->page_name;
+        $data = $request[0]->data;
 
-        $query = "UPDATE $tablename SET data = '$data' WHERE id = " . $id;
+        $query = "UPDATE $tablename SET data = '$data', page_id = $page_id, page_name = '$page_name' WHERE id = " . $id;
         // die($query);
         return $this->db->execute($query);
-    }
-
-    public function update_where($attr, $val, $tablename)
-    {
-        $data = file_get_contents("php://input");
-
-        $condition_values = explode('AND', $val);
-        $condition_attr = explode('AND', $attr);
-
-        $query = "UPDATE $tablename SET values = '$data' ";
-        for ($i = 0; $i < count($condition_attr); $i++) {
-            if ($i == 0) {
-
-            } else {
-                $query .= " AND ";
-            }
-            $query .= "values @> '{\"" . $condition_attr[$i] . "\": \"" . $condition_values[$i] . "\"}'";
-
-        }
-        $query_real = str_replace("%20", " ", $query);
-        // die($query_real);
-
-        return $this->db->execute($query_real);
     }
 
     public function delete($id, $tablename)
