899532e76d92345739dfaa4ec180226158cf5548
Malik <malikabdulaziz51@gmail.com>
2019-12-10 11:55:47 +0700

    page data tes

--- models/page_data.php
+++ models/page_data.php
@@ -72,9 +72,9 @@ class PageData
         $page_id = $request[0]->page_id;
         $page_name = $request[0]->page_name;
         $data = $request[0]->data[0];
-        die(json_encode($data));
+        $data = json_encode($data);
         $query = "INSERT INTO $tablename (page_id, page_name, data)";
-        $query .= " VALUES ('$page_id','$page_name','$data')";
+        $query .= " VALUES ('$page_id','$page_name',$data)";
         // die($query);
         return $this->db->execute($query);
 
