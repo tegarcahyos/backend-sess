77bdd50e36ee7d4864caed3bedde71b49944abff
Malik <malikabdulaziz51@gmail.com>
2019-12-11 08:58:55 +0700

    update config table

--- models/config_list.php
+++ models/config_list.php
@@ -130,7 +130,7 @@ class ConfigList
 
         $query = "INSERT INTO $tablename (name, type_list, object_id, object_name, object_table, page_id, selected_data, detail_page_id)";
         $query .= "VALUES ('$name' , $type_list, NULLIF($object_id, ''), NULLIF('$page_id', ''), '$selected_data','$detail_page_id')";
-        die($query);
+        // die($query);
         return $this->db->execute($query);
 
     }
