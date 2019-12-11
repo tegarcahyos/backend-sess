7ade76fee629b3916f3a17ba3922896a495c868b
Malik <malikabdulaziz51@gmail.com>
2019-12-11 08:58:37 +0700

    update config table

--- models/config_list.php
+++ models/config_list.php
@@ -118,18 +118,18 @@ class ConfigList
         $name = $request[0]->name;
         $type_list = $request[0]->type_list;
         $object_id = $request[0]->object_id;
-        if (empty($object_id)) {
-            $object_id = null;
-        }
+        // if (empty($object_id)) {
+        //     $object_id = null;
+        // }
         $page_id = $request[0]->page_id;
-        if (empty($page_id)) {
-            $page_id = null;
-        }
+        // if (empty($page_id)) {
+        //     $page_id = null;
+        // }
         $selected_data = json_encode($request[0]->selected_data);
         $detail_page_id = $request[0]->detail_page_id;
 
         $query = "INSERT INTO $tablename (name, type_list, object_id, object_name, object_table, page_id, selected_data, detail_page_id)";
-        $query .= "VALUES ('$name' , $type_list, $object_id, '$page_id', '$selected_data','$detail_page_id')";
+        $query .= "VALUES ('$name' , $type_list, NULLIF($object_id, ''), NULLIF('$page_id', ''), '$selected_data','$detail_page_id')";
         die($query);
         return $this->db->execute($query);
 
