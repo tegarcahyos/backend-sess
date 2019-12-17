abb45014cd0398957f29066b78fbbe340a1ea091
Malik <malikabdulaziz51@gmail.com>
2019-12-17 09:51:06 +0700

    page data delete

--- models/page_data.php
+++ models/page_data.php
@@ -127,7 +127,7 @@ class PageData
 
     public function delete($id, $tablename)
     {
-        $query = 'DELETE FROM ' . $tablename . ' WHERE id = ' . $id;
+        $query = "DELETE FROM $tablename WHERE id = '$id'";
         // die($query);
         return $this->db->execute($query);
     }
