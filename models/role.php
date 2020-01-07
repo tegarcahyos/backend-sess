2ba8346b9aa20c4adbf8d4c2cb2f43106a044ec7
Malik <malikabdulaziz51@gmail.com>
2020-01-07 11:44:58 +0700

    coba return

--- models/role.php
+++ models/role.php
@@ -67,9 +67,10 @@ class Role
         $request = json_decode($data);
         $keys = json_decode(json_encode($request[0]), true);
         foreach ($keys as $key => $value) {
-            if (isset($keys[$key])) {
-                die("Kosong");
-            }
+            die(isset($keys[$key]));
+            // if (isset($keys[$key])) {
+            //     die("Kosong");
+            // }
         }
         $name = $request[0]->name;
 
