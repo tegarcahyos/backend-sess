eac4f099644da667a7dd679ed22721b75dc36bc8
Malik <malikabdulaziz51@gmail.com>
2020-01-07 14:04:17 +0700

    coba return

--- models/role.php
+++ models/role.php
@@ -65,6 +65,7 @@ class Role
         $data = file_get_contents("php://input");
         //
         $request = json_decode($data);
+        die(print_r($request));
         $keys = json_decode(json_encode($request[0]), true);
         foreach ($keys as $key => $value) {
             die($keys);
