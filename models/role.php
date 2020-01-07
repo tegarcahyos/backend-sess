57680c696f5f355cbd9f8982402e243d8dfa4c64
Malik <malikabdulaziz51@gmail.com>
2020-01-07 11:42:42 +0700

    coba return

--- models/role.php
+++ models/role.php
@@ -65,8 +65,10 @@ class Role
         $data = file_get_contents("php://input");
         //
         $request = json_decode($data);
-        $key = json_decode(json_encode($request[0]), true);
-        die(print_r(count($key)));
+        $keys = json_decode(json_encode($request[0]), true);
+        foreach ($keys as $key => $value) {
+            die($key);
+        }
         $name = $request[0]->name;
 
         $query = "INSERT INTO $tablename (name)";
