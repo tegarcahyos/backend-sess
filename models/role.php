8bdd4db86fad831d73a1966b1cb21549585cdcd0
Malik <malikabdulaziz51@gmail.com>
2020-01-07 14:10:10 +0700

    coba return

--- models/role.php
+++ models/role.php
@@ -69,7 +69,7 @@ class Role
         $status = "";
         foreach ($array as $key => $value) {
             // die($array);
-            if (is_null($array[$key])) {
+            if (empty($array[$key])) {
                 $status = "$key exists with a value of NULL";
             } else {
                 $status = "LOL";
