4210f33b6302bbdb8c673af412b48e394e3f429e
Malik <malikabdulaziz51@gmail.com>
2020-01-07 11:34:25 +0700

    coba return

---
+++ models/role.php
@@ -0,0 +1,99 @@
+<?php
+
+class Role
+{
+    public $db;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function get($tablename)
+    {
+        $query = "SELECT
+           *
+          FROM
+             $tablename
+          ORDER BY
+            id ASC";
+
+        $result = $this->db->execute($query);
+
+        $num = $result->rowCount();
+
+        if ($num > 0) {
+
+            $data_arr = array();
+
+            while ($row = $result->fetchRow()) {
+                extract($row);
+
+                $data_item = array(
+                    'id' => $id,
+                    'name' => $name,
+                );
+
+                array_push($data_arr, $data_item);
+                $msg = $data_arr;
+            }
+
+        } else {
+            $msg = 'Data Kosong';
+        }
+
+        return $msg;
+    }
+
+    public function findById($id, $tablename)
+    {
+        $query = 'SELECT * FROM ' . $tablename . ' WHERE id = ' . $id . "";
+        $result = $this->db->execute($query);
+        $row = $result->fetchRow();
+        extract($row);
+
+        $data_item = array(
+            'id' => $id,
+            'name' => $name,
+        );
+        return $data_item;
+    }
+
+    public function insert($tablename)
+    {
+        // get data input from frontend
+        $data = file_get_contents("php://input");
+        //
+        $request = json_decode($data);
+        $key = $request[0];
+        die(count(array($key)));
+        $name = $request[0]->name;
+
+        $query = "INSERT INTO $tablename (name)";
+        $query .= "VALUES ('$name')";
+        // die($query);
+        $result = $this->db->execute($query);
+        die(var_dump($result));
+        return $result;
+    }
+
+    public function update($id, $tablename)
+    {
+        // get data input from frontend
+        $data = file_get_contents("php://input");
+        //
+        $request = json_decode($data);
+        $name = $request->name;
+
+        $query = "UPDATE " . $tablename . " SET name = '" . $name . "' WHERE id = " . $id;
+        // die($query);
+        return $this->db->execute($query);
+    }
+
+    public function delete($id, $tablename)
+    {
+        $query = 'DELETE FROM ' . $tablename . ' WHERE id = ' . $id;
+        // die($query);
+        return $this->db->execute($query);
+    }
+}
