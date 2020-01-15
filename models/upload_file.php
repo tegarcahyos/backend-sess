6d5447cb6c923b60825c63c7c629faea84f6a663
Malik <malikabdulaziz51@gmail.com>
2020-01-15 12:54:25 +0700

    tes upload file

--- models/upload_file.php
+++ models/upload_file.php
@@ -28,7 +28,7 @@ class Upload
                 $errors[] = 'File size exceeds limit: ' . $file_name . ' ' . $file_type;
             }
             if (empty($errors)) {
-                move_uploaded_file($file_tmp, '/'+$file);
+                move_uploaded_file($file_tmp, '/' . $file);
                 $query = "INSERT INTO attachment (file_name) VALUES ('$file_name')";
                 $result = $this->db->execute($query);
                 $res = $this->db->affected_rows();
