a98be5e3edbd2c80422e9a6aaf4f2fade5092a55
Malik <malikabdulaziz51@gmail.com>
2020-01-15 15:21:16 +0700

    login

--- models/upload_file.php
+++ models/upload_file.php
@@ -13,7 +13,7 @@ class Upload
     {
         if (isset($_FILES['files'])) {
             $errors = [];
-            $path = $_SERVER['DOCUMENT_ROOT'] . '/uploads/';
+            $path = '/app/pmo-backend/uploads/';
 
             // $all_files = count($_FILES['files']['tmp_name']);
             // for ($i = 0; $i < $all_files; $i++) {
