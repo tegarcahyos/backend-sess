fc237d6b88adc43fb786af3850a9bfe196ef70a3
Malik <malikabdulaziz51@gmail.com>
2020-01-16 13:17:26 +0700

    tes download

--- models/upload_file.php
+++ models/upload_file.php
@@ -70,4 +70,23 @@ class Upload
         }
 
     }
+
+    public function downloadFile($file)
+    {
+        $file = basename($_GET['file']);
+        $file = '/app/pmo-backend/uploads/' . $file;
+
+        if (!file_exists($file)) { // file does not exist
+            die('file not found');
+        } else {
+            header("Cache-Control: public");
+            header("Content-Description: File Transfer");
+            header("Content-Disposition: attachment; filename=$file");
+            header("Content-Type: application/zip");
+            header("Content-Transfer-Encoding: binary");
+
+            // read the file from disk
+            readfile($file);
+        }
+    }
 }
