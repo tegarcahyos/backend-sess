395e1a3984ad11364d437a05d20af11df74394da
gghandoko <requireapps@gmail.com>
2020-01-16 13:40:29 +0700

    backend pmo chat

--- models/upload_file.php
+++ models/upload_file.php
@@ -27,71 +27,52 @@ class Upload
             $file_type = $_FILES['file']['type'];
             $file_size = $_FILES['file']['size'];
             $tmp = explode('.', $_FILES['file']['name']);
-            echo $tmp;
-            // $file_ext = strtolower(end($tmp));
-            // $file = $path . $file_name;
+            // echo $tmp;
+            $file_ext = strtolower(end($tmp));
+            $file = $path . date('d-m-Y h:i:s').$file_ext;
             // $file_date = date('d-m-Y h:i:s');
-            // if ($file_size > 2097152) {
-            //     $errors[] = 'File size exceeds limit: ' . $file_name . ' ' . $file_type;
+            // $fileConcat = $file_name.$file_date;
+            if ($file_size > 2097152) {
+                $errors[] = 'File size exceeds limit: ' . $file_name . ' ' . $file_type;
+            }
+            if (empty($errors)) {
+                move_uploaded_file($file_tmp, $file);
+                //  print_r($file_tmp);
+                $query = "INSERT INTO upload_file (file_name) VALUES ('$file') RETURNING *";
+                die($query);
+                $result = $this->db->execute($query);
+                $num = $result->rowCount();
+
+                // jika ada hasil
+                if ($num > 0) {
+
+                    $data_arr = array();
+
+                    while ($row = $result->fetchRow()) {
+                        extract($row);
+
+                        // Push to data_arr
+
+                        $data_item = array(
+                            'file_name' => $file_name,
+                        );
+
+                        array_push($data_arr, $data_item);
+                        $msg = $data_arr;
+                    }
+
+                } else {
+                    $msg = 'Data Kosong';
+                }
+
+                return $msg;
+            }
             // }
-            // if (empty($errors)) {
-            //     move_uploaded_file($file_tmp, $file);
-            //     $query = "INSERT INTO upload_file (file_name) VALUES ('$file_name') RETURNING *";
-
-            //     $result = $this->db->execute($query);
-            //     $num = $result->rowCount();
-
-            //     // jika ada hasil
-            //     if ($num > 0) {
-
-            //         $data_arr = array();
-
-            //         while ($row = $result->fetchRow()) {
-            //             extract($row);
-
-            //             // Push to data_arr
-
-            //             $data_item = array(
-            //                 'file_name' => $file_name,
-            //             );
-
-            //             array_push($data_arr, $data_item);
-            //             $msg = $data_arr;
-            //         }
-
-            //     } else {
-            //         $msg = 'Data Kosong';
-            //     }
-
-            //     return $msg;
-            // }
-            // // }
-            // if ($errors) {
-            //     print_r($errors);
-            // }
+            if ($errors) {
+                print_r($errors);
+            }
 
         }
 
-    }
-
-    public function downloadFile($id_file)
-    {
-
-        $query = "SELECT * FROM upload_file where id = $id_file";
-        die($query);
-        $file = basename($_GET['file']);
-        $file = '/app/pmo-backend/uploads/' . $file;
-        if (!file_exists($file)) { // file does not exist
-            die('file not found');
-        } else {
-            header("Cache-Control: public");
-            header("Content-Description: File Transfer");
-            header("Content-Disposition: attachment; filename=$file");
-            header("Content-Type: application/zip");
-            header("Content-Transfer-Encoding: binary");
-
-            // read the file from disk
-            readfile($file);
-        }
     }
 }
