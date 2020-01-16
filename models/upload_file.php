d91585064f634882d4fcaa30566ab53293a3c740
gghandoko <requireapps@gmail.com>
2020-01-16 15:25:24 +0700

    backend pmo chat

--- models/upload_file.php
+++ models/upload_file.php
@@ -22,11 +22,11 @@ class Upload
                 mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
         
                 // 16 bits for "time_mid"
-                // mt_rand( 0, 0xffff ),
+                mt_rand( 0, 0xffff ),
         
                 // // 16 bits for "time_hi_and_version",
                 // // four most significant bits holds version number 4
-                // mt_rand( 0, 0x0fff ) | 0x4000,
+                mt_rand( 0, 0x0fff ) | 0x4000,
         
                 // 16 bits, 8 bits for "clk_seq_hi_res",
                 // 8 bits for "clk_seq_low",
