cf48e9798db19a4614953c204c18a509203ee6c0
Malik <malikabdulaziz51@gmail.com>
2020-01-20 13:40:41 +0700

    cors problem

--- router.php
+++ router.php
@@ -129,7 +129,6 @@ class Router
             // --- LOGIN ---
             $r->post('/api/index.php/login', 'Login/authenticate');
             $r->post('/api/index.php/loginApiFactory', 'Login/apiFactory');
-
             // --- CHECK TOKEN ---
             // if (!empty($token)) {
             //     $passed = $this->check_token($token);
@@ -354,6 +353,27 @@ class Router
             // }
 
         });
+
+
+        if (isset($_SERVER['HTTP_ORIGIN'])) {
+            // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+            // you want to allow, and if so:
+            header('Access-Control-Allow-Origin: *');
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Max-Age: 1000');
+        }
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+                // may also be using PUT, PATCH, HEAD etc
+                header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+            }
+        
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+                header("Access-Control-Allow-Headers: Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization");
+            }
+            exit(0);
+        }
+
 
         // Fetch method and URI from somewhere
         $httpMethod = $_SERVER['REQUEST_METHOD'];
