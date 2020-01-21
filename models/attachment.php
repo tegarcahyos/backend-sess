81f82983ff3f10641305a930ee20c2952d50cdea
gghandoko <requireapps@gmail.com>
2020-01-21 11:32:57 +0700

    backend api

--- models/attachment.php
+++ models/attachment.php
@@ -92,6 +92,7 @@ class Attachment
         $query = "SELECT * FROM  $tablename WHERE group_id = $group_id ORDER BY id";
         $result = $this->db->execute($query);
         $num = $result->rowCount();
+        echo $num;
         // die($query);
         if ($num > 0) {
 
