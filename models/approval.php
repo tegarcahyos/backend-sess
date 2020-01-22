17d16f70ce8861c376e6383d1a967ecd3c37a75a
Malik <malikabdulaziz51@gmail.com>
2020-01-22 17:39:49 +0700

    approval

--- models/approval.php
+++ models/approval.php
@@ -27,6 +27,7 @@ class Approval
                 $data_item = array(
                     'id' => $id,
                     'data' => json_decode($data),
+                    'pc_id' => $pc_id,
                 );
 
                 array_push($data_arr, $data_item);
@@ -56,56 +57,12 @@ class Approval
             $data_item = array(
                 'id' => $id,
                 'data' => json_decode($data),
+                'pc_id' => $pc_id,
             );
 
             $msg = $data_item;
             return $msg;
         }
-    }
-
-    public function getByValues($attr, $val, $tablename)
-    {
-
-        $query = "SELECT * FROM $tablename WHERE ";
-        $values = explode('AND', $val);
-        $attr = explode('AND', $attr);
-        for ($i = 0; $i < count($attr); $i++) {
-            if ($i == 0) {
-
-            } else {
-                $query .= " AND ";
-            }
-            // $query .= "values @> '{\"" . $attr[$i] . "\": \"" . $values[$i] . "\"}'";
-            $query .= "values ->> '" . $attr[$i] . "' = '" . $values[$i] . "'";
-
-        }
-
-        $query_real = str_replace("%20", " ", $query);
-        die($query_real);
-        $result = $this->db->execute($query_real);
-
-        $num = $result->rowCount();
-
-        if ($num > 0) {
-            $data_arr = array();
-
-            while ($row = $result->fetchRow()) {
-                extract($row);
-                $data_item = array(
-                    'id' => $id,
-                    'name' => $name,
-                    'values' => json_decode($values),
-                );
-
-                array_push($data_arr, $data_item);
-                $msg = $data_arr;
-            }
-
-        } else {
-            $msg = 'Data Kosong';
-        }
-
-        return $msg;
     }
 
     public function insert($tablename)
@@ -115,9 +72,10 @@ class Approval
         $request = json_decode($data);
         // die(print_r($request));
         $data = json_encode($request[0]->data);
+        $pc_id = $request[0]->pc_id;
 
-        $query = 'INSERT INTO ' . $tablename . ' (data) ';
-        $query .= "VALUES ('$data') RETURNING *";
+        $query = 'INSERT INTO ' . $tablename . ' (data, pc_id) ';
+        $query .= "VALUES ('$data', '$pc_id') RETURNING *";
         // die($query);
         $result = $this->db->execute($query);
         $num = $result->rowCount();
@@ -135,6 +93,7 @@ class Approval
                 $data_item = array(
                     'id' => $id,
                     'data' => json_decode($data),
+                    'pc_id' => $pc_id,
                 );
 
                 array_push($data_arr, $data_item);
@@ -157,8 +116,9 @@ class Approval
 
         $request = json_decode($data);
         $data = json_encode($request[0]->data);
+        $pc_id = $request[0]->pc_id;
 
-        $query = "UPDATE $tablename SET data = '$data' WHERE id = '$id' RETURNING *";
+        $query = "UPDATE $tablename SET data = '$data', pc_id = '$pc_id' WHERE id = '$id' RETURNING *";
 
         // die($query);
 
@@ -178,6 +138,7 @@ class Approval
                 $data_item = array(
                     'id' => $id,
                     'data' => json_decode($data),
+                    'pc_id' => $pc_id,
                 );
 
                 array_push($data_arr, $data_item);
@@ -204,26 +165,6 @@ class Approval
         } else {
             return $msg = array("message" => 'Data tidak ditemukan', "code" => 400);
         }
-    }
-
-    public function deleteByValues($attr, $val, $tablename)
-    {
-
-        $condition_values = explode('AND', $val);
-        $condition_attr = explode('AND', $attr);
-        $query = "DELETE FROM  $tablename";
-        for ($i = 0; $i < count($condition_attr); $i++) {
-            if ($i == 0) {
-
-            } else {
-                $query .= " AND ";
-            }
-            $query .= "values @> '{\"" . $condition_attr[$i] . "\": \"" . $condition_values[$i] . "\"}'";
-
-        }
-        $query_real = str_replace("%20", " ", $query);
-        // die($query_real);
-        return $this->db->execute($query_real);
     }
 
 }
