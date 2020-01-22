64147fcef717fdd6c27a927baafd4f8ac3c00b90
Malik <malikabdulaziz51@gmail.com>
2020-01-22 10:22:32 +0700

    response message

--- router.php
+++ router.php
@@ -588,7 +588,7 @@ class Router
 
         try {
             if ($result == [] || $result === 'Data Kosong' || $result == '0') {
-                $this->msg(http_response_code(204), 204, $result, "gagal", 0);
+                $this->msg(http_response_code(404), 404, $result, "gagal", 0);
             } else if ($result == "404") {
                 $this->msg(http_response_code(404), 404, 'Page Not Found', "gagal", 0);
             } else if ($result == "405") {
