c2947d8c66c98c21db25740872bae582da147881
gghandoko <requireapps@gmail.com>
2020-01-23 11:47:44 +0700

    pmo backendie

--- models/periode.php
+++ models/periode.php
@@ -161,7 +161,7 @@ class Periode
                 extract($row);
 
                 $data_item = array(
-                    'id_per' => $id_per,                    
+                    'id_per' => $id,                    
                 );
                 $msg_item = $data_item;
             }
