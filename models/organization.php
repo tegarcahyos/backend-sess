da9bdefda6b5977fe1af0732483fdb223eb73f7f
Malik <malikabdulaziz51@gmail.com>
2020-02-03 14:24:30 +0700

    WEW

--- models/organization.php
+++ models/organization.php
@@ -104,8 +104,8 @@ class Organization
         $result = $this->db->execute($get_unit);
         // die(print_r($result));
         $row = $result->fetchRow();
-        // die($row['exists'] != 't');
-        if ($row['exists'] != 't') {
+        die($row['exists'] == 't');
+        if ($row['exists'] == 't') {
             die("403");
         }
         $query = "DELETE FROM $tablename WHERE id = '$id'";
