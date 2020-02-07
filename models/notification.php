20969512a057b981cbf1593c02c1cee02c262c2a
Malik <malikabdulaziz51@gmail.com>
2020-02-07 14:53:02 +0700

    check notif

--- models/notification.php
+++ models/notification.php
@@ -44,6 +44,7 @@ class Notification
     public function findById($id, $tablename)
     {
         $query = "SELECT * FROM $tablename WHERE unit_id_or_user_id = '$id' AND status = 0";
+        die($query);
         $result = $this->db->execute($query);
         $num = $result->rowCount();
 
