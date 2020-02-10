1aa01f0f6a1ac7c8a934924ef71a39cdd10c1670
Malik <malikabdulaziz51@gmail.com>
2020-02-10 13:45:01 +0700

    ej

--- models/program_charter.php
+++ models/program_charter.php
@@ -474,9 +474,11 @@ class ProgramCharter
         $string = str_replace(']', "", $string);
         $string = str_replace('"', "", $string);
         $explode = explode(', ', $string);
+
+        // die(gettype());
         for ($i = 0; $i < count($data_arr); $i++) {
-            $key = array_search($id, $explode);
-            // die($key);
+            $key = array_search('e204e2e0-ea96-43f0-9811-43dce22cc7ac', $explode);
+            die($key);
             if ($key !== false) {
                 die('true');
                 unset($explode["\"$key\""]);
