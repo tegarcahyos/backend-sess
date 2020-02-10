6a0c69c91a18b3ae46bf11e48cf5f4eb8f01aebf
Malik <malikabdulaziz51@gmail.com>
2020-02-10 11:51:13 +0700

    tes tis

---
+++ models/expert_judgement.php
@@ -0,0 +1,236 @@
+<?php
+
+class ExpertJudgement
+{
+    public $db;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function get($tablename)
+    {
+        $query = "SELECT * FROM  $tablename ";
+        // die($query);
+        $result = $this->db->execute($query);
+        // hitung result
+        $num = $result->rowCount();
+
+        if ($num > 0) {
+
+            $data_arr = array();
+
+            while ($row = $result->fetchRow()) {
+                extract($row);
+
+                $data_item = array(
+                    'id' => $id,
+                    'user_id' => $user_id,
+                    'program_charter' => $program_charter,
+                );
+
+                array_push($data_arr, $data_item);
+                $msg = $data_arr;
+            }
+
+        } else {
+            $msg = 'Data Kosong';
+        }
+
+        return $msg;
+    }
+
+    public function findById($id, $tablename)
+    {
+        $query = "SELECT * FROM $tablename WHERE id = '$id'";
+        $result = $this->db->execute($query);
+        if (empty($result)) {
+            $msg = array("message" => 'Data Tidak Ditemukan', "code" => 400);
+            return $msg;
+        } else {
+            $row = $result->fetchRow();
+            extract($row);
+
+            // Push to data_arr
+
+            $data_item = array(
+                'id' => $id,
+                'user_id' => $user_id,
+                'program_charter' => $program_charter,
+            );
+
+            $msg = $data_item;
+            return $msg;
+        }
+    }
+
+    public function findByUserId($user_id, $tablename)
+    {
+        $query = "SELECT * FROM $tablename WHERE user_id = '$user_id'";
+        $result = $this->db->execute($query);
+        if (empty($result)) {
+            $msg = array("message" => 'Data Tidak Ditemukan', "code" => 400);
+            return $msg;
+        } else {
+            $row = $result->fetchRow();
+            extract($row);
+
+            // Push to data_arr
+
+            $data_item = array(
+                'id' => $id,
+                'user_id' => $user_id,
+                'program_charter' => $program_charter,
+            );
+
+            $msg = $data_item;
+            return $msg;
+        }
+    }
+
+    public function insert($tablename)
+    {
+        $data = file_get_contents("php://input");
+        //
+        $request = json_decode($data);
+
+        $user_id = $request[0]->user_id;
+        $program_charter = json_encode($request[0]->program_charter);
+
+        $variable = array('user_id', 'program_charter');
+        foreach ($variable as $item) {
+            if (!isset($request[0]->{$item})) {
+                return "402";
+            }
+
+            $$item = $request[0]->{$item};
+        }
+
+        // die($program_charter);
+
+        $query = 'INSERT INTO ' . $tablename . ' (user_id, program_charter) ';
+        $query .= "VALUES ('$user_id', '$program_charter') RETURNING *";
+        // die($query);
+        $result = $this->db->execute($query);
+        if (empty($result)) {
+            return "402";
+        } else {
+            $num = $result->rowCount();
+
+            // jika ada hasil
+            if ($num > 0) {
+
+                $data_arr = array();
+
+                while ($row = $result->fetchRow()) {
+                    extract($row);
+
+                    // Push to data_arr
+
+                    $data_item = array(
+                        'id' => $id,
+                        'user_id' => $user_id,
+                        'program_charter' => $program_charter,
+                    );
+
+                    array_push($data_arr, $data_item);
+                    $msg = $data_arr;
+                }
+
+            }
+        }
+
+        return $msg;
+
+    }
+
+    public function update($id, $tablename)
+    {
+        // init attribute dan values
+
+        $data = file_get_contents("php://input");
+
+        $request = json_decode($data);
+
+        $variable = array('user_id', 'program_charter');
+        foreach ($variable as $item) {
+            if (!isset($request[0]->{$item})) {
+                return "402";
+            }
+
+            $$item = $request[0]->{$item};
+        }
+
+        $program_charter = json_encode($program_charter);
+        $program_charter = str_replace('"', "", $program_charter);
+        $program_charter = str_replace('[', "", $program_charter);
+        $program_charter = str_replace(']', "", $program_charter);
+
+        $query = "UPDATE $tablename SET user_id = '$user_id', program_charter = {'$program_charter'} WHERE id = '$id' RETURNING *";
+
+        // die($query);
+
+        $result = $this->db->execute($query);
+        if (empty($result)) {
+            return "402";
+        } else {
+            $num = $result->rowCount();
+
+            // jika ada hasil
+            if ($num > 0) {
+
+                $data_arr = array();
+
+                while ($row = $result->fetchRow()) {
+                    extract($row);
+
+                    // Push to data_arr
+
+                    $data_item = array(
+                        'id' => $id,
+                        'user_id' => $user_id,
+                        'program_charter' => $program_charter,
+                    );
+
+                    array_push($data_arr, $data_item);
+                    $msg = $data_arr;
+                }
+
+            }
+        }
+
+        return $msg;
+    }
+
+    public function delete($id, $tablename)
+    {
+        $query = "DELETE FROM $tablename WHERE id = '$id'";
+
+        $result = $this->db->execute($query);
+        // return $result;
+        $res = $this->db->affected_rows();
+
+        if ($res == true) {
+            return $msg = array("message" => 'Data Berhasil Dihapus', "code" => 200);
+        } else {
+            return $msg = array("message" => 'Data tidak ditemukan', "code" => 400);
+        }
+    }
+
+    public function deleteByUserId($user_id, $tablename)
+    {
+        $query = "DELETE FROM $tablename WHERE user_id = '$user_id'";
+
+        $result = $this->db->execute($query);
+        // return $result;
+        $res = $this->db->affected_rows();
+
+        if ($res == true) {
+            return $msg = array("message" => 'Data Berhasil Dihapus', "code" => 200);
+        } else {
+            return $msg = array("message" => 'Data tidak ditemukan', "code" => 400);
+        }
+    }
+
+}
