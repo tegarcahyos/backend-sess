86bffce643d1d3ea8902cb8396ab642ce7962993
Malik <malikabdulaziz51@gmail.com>
2020-02-10 13:10:25 +0700

    ej

--- models/program_charter.php
+++ models/program_charter.php
@@ -470,13 +470,11 @@ class ProgramCharter
             }
         }
         // die(print_r($data_arr[1]['program_charter']));
-        $array = array($data_arr[1]['program_charter']);
-        die(array_search('' . $id . '', $array));
 
         for ($i = 0; $i < count($data_arr); $i++) {
-            $data = json_decode($data_arr[0]['program_charter']);
-            if (($key = array_search($id, $data)) !== false) {
-                unset($data[$key]);
+            $array = array($data_arr[1]['program_charter']);
+            if (($key = array_search($id, $array)) !== false) {
+                unset($array[$key]);
             }
         }
 
