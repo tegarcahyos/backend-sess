d594bce52abc11ffad6012b0a7d6ec2b27665c82
Malik <malikabdulaziz51@gmail.com>
2020-02-10 12:58:32 +0700

    ej

--- models/program_charter.php
+++ models/program_charter.php
@@ -471,7 +471,7 @@ class ProgramCharter
         }
 
         for ($i = 0; $i < count($data_arr); $i++) {
-            $data = str_replace('"', "", $data_arr[$i]['program_charter']);
+            $data = $data_arr[$i]['program_charter'];
             die(gettype($data));
             if (($key = array_search($id, $data)) !== false) {
                 unset($data[$key]);
