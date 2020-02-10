5b8e1f1fe942793ef1494b71dcd9f9e148f862c1
Malik <malikabdulaziz51@gmail.com>
2020-02-10 13:43:38 +0700

    ej

--- models/program_charter.php
+++ models/program_charter.php
@@ -477,7 +477,7 @@ class ProgramCharter
 
         die(gettype('e204e2e0-ea96-43f0-9811-43dce22cc7ac'));
         for ($i = 0; $i < count($data_arr); $i++) {
-            $key = array_search(, $explode);
+            // $key = array_search(, $explode);
             // die($key);
             if ($key !== false) {
                 die('true');
