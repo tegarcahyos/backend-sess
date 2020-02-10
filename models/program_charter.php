be9161994b47c5314659558f2ca5a2465d15571c
Malik <malikabdulaziz51@gmail.com>
2020-02-10 13:21:58 +0700

    ej

--- models/program_charter.php
+++ models/program_charter.php
@@ -473,6 +473,11 @@ class ProgramCharter
         $string = str_replace('[', "", $string);
         $string = str_replace(']', "", $string);
         $explode = explode(', ', $string);
+        if (($key = array_search('' . $id . '', $array)) !== false) {
+            die("true");
+        } else {
+            die("false");
+        }
         die(print_r($explode));
         if (($key = array_search('' . $id . '', $array)) !== false) {
             die("true");
