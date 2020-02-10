48fa964a8cb4efbfa6ee58ce7c02eaecdf345e45
Malik <malikabdulaziz51@gmail.com>
2020-02-10 14:32:58 +0700

    ej

--- models/program_charter.php
+++ models/program_charter.php
@@ -486,7 +486,7 @@ class ProgramCharter
                 unset($explode[$key]);
             }
 
-            die($explode);
+            die(print_r($explode));
 
             $update = "UPDATE expert_judgement SET program_charter = $explode WHERE program_charter LIKE '%$id_pc%'";
             $this->db->execute($update);
