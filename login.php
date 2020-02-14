45a597c3b28e01cef868c1a27731d649da805039
Malik <malikabdulaziz51@gmail.com>
2020-02-14 09:49:34 +0700

    tes tes

--- login.php
+++ login.php
@@ -52,8 +52,8 @@ class Login
             $name = $row['name'];
             $password2 = $row['password'];
         }
-        // die($password2);
-        die(password_verify($password2));
+        die($password2);
+        // die(password_verify($password2));
         if (password_verify($password, $password2)) {
             $secret_key = "YOUR_SECRET_KEY";
             $issuer_claim = "THE_ISSUER"; // this can be the servername
