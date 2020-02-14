240e80228529fbb4d543f6b8a5c14e5856a2e585
Malik <malikabdulaziz51@gmail.com>
2020-02-14 17:26:59 +0700

    tes migrate connection

--- models/transformer_connect.php
+++ models/transformer_connect.php
@@ -15,7 +15,12 @@ class TransformerStaging
         // $this->db_transformer = newADOConnection('mysqli');
         // $this->db_transformer->connect($db_host, $db_username, $db_password, $db_name_transformer);
         $mysqli = mysqli_connect($db_host, $db_username, $db_password, $db_name_transformer);
-        die(print_r($mysqli));
+        if (mysqli_connect_errno()) {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+            exit();
+        } else {
+            die(print_r($mysqli));
+        }
 
         return $this->db_transformer;
     }
