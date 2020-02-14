8c3a0c41078017914ff2fda68dab361c4ff32532
Malik <malikabdulaziz51@gmail.com>
2020-02-14 18:01:44 +0700

    tes migrate connection

--- models/migrate_staging.php
+++ models/migrate_staging.php
@@ -51,8 +51,6 @@ class MigrateStaging
                     'status' => $status,
                     'generator_id' => $generator_id,
                 );
-
-                die(print_r(count($data_item)));
                 for ($i = 0; $i < count($data_item); $i++) {
                     $title = $data_item[$i]['title'];
                     $strategic_initiative = $data_item[$i]['strategic_initiative'];
