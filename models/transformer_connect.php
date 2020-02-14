ff423f1062e19d02c47dc464a111f336a4065e2e
Malik <malikabdulaziz51@gmail.com>
2020-02-14 17:23:12 +0700

    tes migrate connection

---
+++ models/transformer_connect.php
@@ -0,0 +1,21 @@
+<?php
+require __DIR__ . "/../adodb/adodb.inc.php";
+
+class TransformerStaging
+{
+    public $db_transformer;
+
+    public function transformer_connect()
+    {
+
+        $db_host = '10.62.163.49';
+        $db_username = 'transformer';
+        $db_password = 'transform$001';
+        $db_name_transformer = 'transformer';
+        $this->db_transformer = newADOConnection('mysqli');
+        $this->db_transformer->connect($db_host, $db_username, $db_password, $db_name_transformer);
+        die(print_r($this->db_transformer));
+
+        return $this->db_transformer;
+    }
+}
