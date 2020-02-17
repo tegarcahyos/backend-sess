8476756d5881da3b76e588a851ad994683ba6e98
Malik <malikabdulaziz51@gmail.com>
2020-02-17 09:25:21 +0700

    ahp judgement featured program charter

---
+++ models/ahp_featured_program_charter.php
@@ -0,0 +1,184 @@
+<?php
+
+class AHPFeaturedPC
+{
+    public $db;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function get($tablename)
+    {
+        $query = "SELECT
+           *
+          FROM
+             $tablename";
+
+        // die($query);
+        $result = $this->db->execute($query);
+
+        $num = $result->rowCount();
+
+        if ($num > 0) {
+
+            $data_arr = array();
+
+            while ($row = $result->fetchRow()) {
+                extract($row);
+
+                $data_item = array(
+                    'id' => $id,
+                    'organization_id' => $organization_id,
+                    'data' => json_decode($data),
+                    'judgement' => json_decode($judgement),
+                );
+
+                array_push($data_arr, $data_item);
+                $msg = $data_arr;
+            }
+
+        } else {
+            $msg = 'Data Kosong';
+        }
+
+        return $msg;
+    }
+
+    public function findById($id, $tablename)
+    {
+        $query = "SELECT * FROM $tablename WHERE id = '$id'";
+        $result = $this->db->execute($query);
+        $row = $result->fetchRow();
+        if (is_bool($row)) {
+            $msg = "Data Kosong";
+            return $msg;
+        } else {
+            extract($row);
+
+            $data_item = array(
+                'id' => $id,
+                'organization_id' => $organization_id,
+                'data' => json_decode($data),
+                'judgement' => json_decode($judgement),
+            );
+            return $data_item;
+        }
+    }
+
+    public function insert($tablename)
+    {
+        // get data input from frontend
+        $data = file_get_contents("php://input");
+        //
+        $request = json_decode($data);
+
+        $variable = array('organization_id', 'data', 'judgement');
+        foreach ($variable as $item) {
+            if (!isset($request[0]->{$item})) {
+                return "422";
+            }
+
+            $$item = $request[0]->{$item};
+        }
+
+        $data = json_encode($data);
+        $judgement = json_encode($judgement);
+
+        $query = "INSERT INTO $tablename (organization_id, data, judgement)";
+        $query .= "VALUES ('$organization_id', '$data', '$judgement') RETURNING *";
+        // die($query);
+        $result = $this->db->execute($query);
+        if (empty($result)) {
+            return "422";
+        } else {
+            $num = $result->rowCount();
+
+            if ($num > 0) {
+
+                $data_arr = array();
+
+                while ($row = $result->fetchRow()) {
+                    extract($row);
+
+                    $data_item = array(
+                        'id' => $id,
+                        'organization_id' => $organization_id,
+                        'data' => json_decode($data),
+                        'judgement' => json_decode($judgement),
+                    );
+
+                    array_push($data_arr, $data_item);
+                    $msg = $data_arr;
+                }
+
+            }
+        }
+        return $msg;
+    }
+
+    public function update($id, $tablename)
+    {
+        // get data input from frontend
+        $data = file_get_contents("php://input");
+        //
+        $request = json_decode($data);
+        $variable = array('organization_id', 'data', 'judgement');
+        foreach ($variable as $item) {
+            if (!isset($request[0]->{$item})) {
+                return "422";
+            }
+
+            $$item = $request[0]->{$item};
+        }
+
+        $data = json_encode($data);
+        $judgement = json_encode($judgement);
+
+        $query = "UPDATE $tablename SET organization_id = '$organization_id', data = '$data', judgement = '$judgement' WHERE id = '$id' RETURNING *";
+        // die($query);
+        $result = $this->db->execute($query);
+        if (empty($result)) {
+            return "422";
+        } else {
+            $num = $result->rowCount();
+
+            if ($num > 0) {
+
+                $data_arr = array();
+
+                while ($row = $result->fetchRow()) {
+                    extract($row);
+
+                    $data_item = array(
+                        'id' => $id,
+                        'organization_id' => $organization_id,
+                        'data' => json_decode($data),
+                        'judgement' => json_decode($judgement),
+                    );
+
+                    array_push($data_arr, $data_item);
+                    $msg = $data_arr;
+                }
+
+            }
+        }
+        return $msg;
+    }
+
+    public function delete($id, $tablename)
+    {
+        $query = "DELETE FROM $tablename WHERE id = '$id'";
+        // die($query);
+        $result = $this->db->execute($query);
+        // return $result;
+        $res = $this->db->affected_rows();
+
+        if ($res == true) {
+            return $msg = array("message" => 'Data Berhasil Dihapus', "code" => 200);
+        } else {
+            return $msg = "Data Kosong";
+        }
+    }
+}
