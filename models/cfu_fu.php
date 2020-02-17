90fca71d373a7b423841c847d252d6f0f5d9478d
Malik <malikabdulaziz51@gmail.com>
2020-02-17 17:41:54 +0700

    tes tes

--- models/cfu_fu.php
+++ models/cfu_fu.php
@@ -122,7 +122,7 @@ class CfuFu
                 );
 
                 array_push($unitArray, $data_item);
-
+                die(print_r($unitArray));
                 for ($i = 0; $i < count($unitArray); $i++) {
                     $user = "SELECT * FROM user_detail WHERE unit_id = '" . $unitArray[$i]['id'] . "'";
                     $listUser = $this->db->execute($user);
