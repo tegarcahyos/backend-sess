909d51b82e370470e24eadf1fe3f47faacf719c4
Malik <malikabdulaziz51@gmail.com>
2020-02-18 10:13:57 +0700

    staging

--- models/migrate_staging.php
+++ models/migrate_staging.php
@@ -63,8 +63,12 @@ class MigrateStaging
                 $row = $data_generator->fetchRow();
                 $name = $row['name'];
 
-                $query_staging = "INSERT IGNORE INTO staging_program (btp, businessRisk, description, title, generator, programType)";
-                $query_staging .= "VALUES (1, '$risks', '$description', '$title', '$name', 'btp')";
+                $query_get = "SELECT EXISTS(SELECT * FROM staging_program WHERE title = '$title'";
+                
+                die(print_r($query_get));
+
+                // $query_staging = "INSERT IGNORE INTO staging_program (btp, businessRisk, description, title, generator, programType)";
+                // $query_staging .= "VALUES (1, '$risks', '$description', '$title', '$name', 'btp')";
 
                 // $query_staging = "INSERT INTO staging_program (btp, businessRisk, description, title, generator, programType)
                 // SELECT * FROM (SELECT 1, '$risks', '$description', '$title', '$name', 'btp') AS tmp
