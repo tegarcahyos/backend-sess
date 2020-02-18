53ddeffc346092d3ab4ccc554ea25aad1bd4fe5c
Malik <malikabdulaziz51@gmail.com>
2020-02-18 14:06:42 +0700

    tes get pc by cfu fu

--- models/program_charter.php
+++ models/program_charter.php
@@ -112,12 +112,11 @@ class ProgramCharter
             for ($j = 0; $j < count($pc_arr); $j++) {
                 if ($pc_arr[$j]['unit_id'] === $unit_arr[$i]['unit_id']) {
                     array_push($resultPc, $pc_arr[$j]);
-                    // echo json_encode($pc_arr[$j]['unit_id']) . "===" . json_encode($unit_arr[$i]['unit_id']);
                 }
             }
         }
 
-        die(print_r($resultPc));
+        return $resultPc;
 
     }
 
