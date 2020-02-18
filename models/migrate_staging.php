5a33c47789155202fed80eac70360bfd0b037844
Malik <malikabdulaziz51@gmail.com>
2020-02-18 11:09:04 +0700

    staging

--- models/migrate_staging.php
+++ models/migrate_staging.php
@@ -69,13 +69,14 @@ class MigrateStaging
                 if ($get_result == false) {
                     $query_staging = "INSERT INTO staging_program (btp, businessRisk, description, title, generator, programType)";
                     $query_staging .= "VALUES (1, '$risks', '$description', '$title', '$name', 'btp')";
-                    // die($query_staging);
-                    $db_transformer->execute($query_staging);
                 } elseif ($get_result == true) {
                     $query_staging = "UPDATE staging_program SET btp = 1, businessRisk = '$risks', description = '$description', title = '$title', generator = '$name', programType = 'btp' WHERE title = '$title' AND generator = '$name'";
                     // die($query_staging);
-                    $db_transformer->execute($query_staging);
+
                 }
+
+                die($query_staging);
+                $db_transformer->execute($query_staging);
 
                 // $query_staging = "INSERT INTO staging_program (btp, businessRisk, description, title, generator, programType)
                 // SELECT * FROM (SELECT 1, '$risks', '$description', '$title', '$name', 'btp') AS tmp
