b8307b44c663cede2c612d12b62257a8a017a54b
Malik <malikabdulaziz51@gmail.com>
2020-02-20 15:11:03 +0700

    tes get pc by unit and periode

--- models/program_charter.php
+++ models/program_charter.php
@@ -211,7 +211,7 @@ class ProgramCharter
                 for ($i = 0; $i < count($unitArray); $i++) {
                     for ($l = 0; $l < count($siArr); $l++) {
                         $pc = "SELECT * FROM program_charter WHERE unit_id = '" . $unitArray[$i]['id'] . "' AND strategic_initiative = '" . $siArr[$l]['id'] . "'";
-                        // echo $pc;
+                        echo $pc;
                         $listPC = $this->db->execute($pc);
                         $num = $listPC->rowCount();
 
