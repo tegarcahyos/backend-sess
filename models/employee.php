1882ebbeb91f91f2eb741e37a4f02422b91849a4
Malik <malikabdulaziz51@gmail.com>
2020-02-21 09:35:46 +0700

    find employee by id

--- models/employee.php
+++ models/employee.php
@@ -92,4 +92,39 @@ class Employee
         return $msg;
     }
 
+    public function findById($id)
+    {
+        $query = "SELECT DISTINCT * FROM employee  WHERE id = $id";
+        // die($query);
+        $result = $this->db->execute($query);
+        if (empty($result)) {
+            $msg = "Data Kosong";
+        } else {
+            $row = $result->fetchRow();
+            extract($row);
+
+            $data_item = array(
+                'c_company_code' => $c_company_code,
+                'v_company_code' => $v_company_code,
+                'c_kode_divisi' => $c_kode_divisi,
+                'v_short_divisi' => $v_short_divisi,
+                'c_kode_unit' => $c_kode_unit,
+                'v_short_unit' => $v_short_unit,
+                'v_long_unit' => $v_long_unit,
+                'objidposisi' => $objidposisi,
+                'c_kode_posisi' => $c_kode_posisi,
+                'v_short_posisi' => $v_short_posisi,
+                'v_long_posisi' => $v_long_posisi,
+                'c_flag_chief' => $c_flag_chief,
+                'n_nik' => $n_nik,
+                'v_nama_karyawan' => $v_nama_karyawan,
+                'v_jenis_kelamin' => $v_jenis_kelamin,
+                'v_personnel_subarea' => $v_personnel_subarea,
+            );
+            $msg = $data_item;
+        }
+
+        return $msg;
+    }
+
 }
