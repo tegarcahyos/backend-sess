f9a1833778bbf97b8308ea147647ed323084e7e3
Malik <malikabdulaziz51@gmail.com>
2020-02-25 16:44:56 +0700

    tes get permissions

--- models/permissions.php
+++ models/permissions.php
@@ -56,7 +56,15 @@ class Permissions
 
     public function findByRole($id, $tablename)
     {
-        $query = "SELECT * FROM  $tablename WHERE role_id = '$id'";
+        $query = "SELECT
+            id, 
+            features_id, 
+            role_id, 
+            case when read = true then 'true' else 'false' end as read,
+            case when write = true then 'true' else 'false' end as write,
+            case when delete = true then 'true' else 'false' end as delete,
+            case when approve = true then 'true' else 'false' end as approve
+         FROM  $tablename WHERE role_id = '$id'";
         // die($query);
         $result = $this->db->execute($query);
         // hitung result
@@ -92,7 +100,15 @@ class Permissions
 
     public function findById($id, $tablename)
     {
-        $query = "SELECT * FROM $tablename WHERE id = '$id'";
+        $query = "SELECT 
+        id, 
+        features_id, 
+        role_id, 
+        case when read = true then 'true' else 'false' end as read,
+        case when write = true then 'true' else 'false' end as write,
+        case when delete = true then 'true' else 'false' end as delete,
+        case when approve = true then 'true' else 'false' end as approve 
+        FROM $tablename WHERE id = '$id'";
         $result = $this->db->execute($query);
         $row = $result->fetchRow();
         if (is_bool($row)) {
