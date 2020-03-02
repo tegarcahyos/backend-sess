0fa86c3ab20dc826d32c3b934fba725d503bfeac
Malik <malikabdulaziz51@gmail.com>
2020-03-02 11:06:41 +0700

    tes org and periode

--- models/expert_judgement.php
+++ models/expert_judgement.php
@@ -126,7 +126,7 @@ class ExpertJudgement
         // die(print_r($getOrg));
 
         for ($m = 0; $m < count($getOrg); $m++) {
-            $query = "SELECT * FROM  $tablename WHERE periode_id = '$id' AND unit_id = '" . $getOrg[$m]['id'] . "'";
+            $query = "SELECT * FROM  $tablename WHERE periode_id = '$periode_id' AND unit_id = '" . $getOrg[$m]['id'] . "'";
             // die($query);
             $result = $this->db->execute($query);
             // hitung result
