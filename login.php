ef72b1852795c1fac4f066a8a894179e34b31173
Malik <malikabdulaziz51@gmail.com>
2020-03-06 11:06:38 +0700

    tes request account

--- login.php
+++ login.php
@@ -28,7 +28,7 @@ class Login
             $get_user = "SELECT * FROM $tablename WHERE username = '$username'";
             $result = $this->db->execute($query);
             $row_get = $result->fetchRow();
-            die(print_r(empty($row_get['password'])));
+            die(print_r(!empty($row_get['password'])));
             if (empty($row_get['password'])) {
                 // echo "goblok";
                 $password_hash = password_hash($password, PASSWORD_BCRYPT);
