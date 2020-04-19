de6afaf8670b2321e70b4e4dd347096c2ac1b6ee
Malik <malikabdulaziz51@gmail.com>
2020-04-20 05:55:33 +0700

    tes quadran get by org and periode

--- models/expert_judgement.php
+++ models/expert_judgement.php
@@ -181,8 +181,8 @@ class ExpertJudgement
                         }
                     }
                     // array_push($result_arr, $data_item);
-                    $data_arr;
                 }
+                $data_arr;
             }
             $msg = $data_arr;
         } else {
